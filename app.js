d85ec4c3363b157c6781914859c2f968f0a98906
walid <taktak.walid@gmail.com>
2023-11-19 15:59:57 +0100

    commit new version

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
 const express=require('express');
-const mongoose =require("mongoose")
+const  mongoose =require("mongoose")
 const dotenv =require('dotenv')
-
+const categorieRouter=require("./routes/categorie.route")
+const scategorieRouter=require("./routes/scategorie.route")
+const articleRouter=require("./routes/article.route")
 dotenv.config()
 const app = express();
-const categorieRouter =require("./routes/categories.route")
-const scategorieRouter =require("./routes/scategorie.route")
-app.use('/api/scategories', scategorieRouter);
-app.use('/api/categories', categorieRouter);
-const articleRouter =require("./routes/article.route")
-app.use('/api/articles', articleRouter);
-//BodyParser Middleware
-app.use(express.json());
-mongoose.set('strictQuery', true)
-const connect = async () => {
-    try {
-    await mongoose.connect(process.env.DATABASEcloud);
-    console.log("Connected to mongoDB.");
-    } catch (error) {
-    throw error;
-    }
-    };
-    mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected!");
-    });
-    
-    app.get("/",(req,res)=>{
-    res.send("bonjour");
-    });
-    app.listen(process.env.PORT, () => {
-        connect();
-    console.log(`Server is listening on port ${process.env.PORT}`); });
-    module.exports = app;
+app.use(express.json())
 
+mongoose.set("strictQuery", false);
+// Connexion à la base données
+mongoose.connect(process.env.DATABASECLOUD,{
+useNewUrlParser: true,
+useUnifiedTopology: true
+})
+.then(() => {console.log("Connexion à la base de données réussie");
+}).catch(err => {
+console.log('Impossible de se connecter à la base de données', err);
+process.exit();
+});
+
+app.use("/api/categories",categorieRouter)
+app.use("/api/scategories",scategorieRouter)
+app.use("/api/articles",articleRouter)
+app.listen(process.env.PORT, () => {
     
+ console.log(`Server is listening on port ${process.env.PORT}`); });
+ module.exports = app;
